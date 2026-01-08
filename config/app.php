1b5a3ea516ba0637d35afd4b99ffb4b2d8fd03ae
Shift <shift@laravelshift.com>
2026-01-08 22:50:20 +0000

    Shift config files

--- config/app.php
+++ config/app.php
@@ -172,7 +172,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Laravel\Tinker\TinkerServiceProvider::class,
 
         /*
          * Application Service Providers...
