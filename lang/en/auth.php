f256810fee9c3334ba0297b7dff8bb95795d32f8
Shift <shift@laravelshift.com>
2026-01-18 23:14:51 +0000

    Slim `lang` files

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,8 +13,6 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
     'validationMessage' => 'Your account has been created and a validation e-mail has been sent to your e-mail address.  Please click on the link provided to active your account.',
     'validationWarning' => 'You have not yet validated your account. Please click on the link that has been sent to your e-mail address. If you cannot locate our e-mail, please check your junk mail folder.',
     'validationConfirm' => 'Thank you for validating your address.',
