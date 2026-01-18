b7a87d35a8b606a540b42dba8399fb1d76a597cd
Shift <shift@laravelshift.com>
2026-01-18 23:43:43 +0000

    Re-register service providers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,11 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders()
+    ->withProviders([
+        \NZTim\Mailchimp\MailchimpServiceProvider::class,
+        \Intervention\Image\ImageServiceProvider::class,
+        \Maatwebsite\Excel\ExcelServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         // api: __DIR__.'/../routes/api.php',
--- config/app.php
+++ config/app.php
@@ -11,24 +11,6 @@ return [
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        NZTim\Mailchimp\MailchimpServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
