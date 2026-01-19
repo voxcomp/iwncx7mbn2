1a5600bee2a6e9eae9c8f4523396e780c78c6c8e
voxcomp <233089562+voxcomp@users.noreply.github.com>
2026-01-19 17:58:54 -0600

    Updated to Vite

---
+++ resources/js/theme.js
@@ -0,0 +1,6 @@
+import './imagesloaded.pkgd.min.js';
+import './masonry.pkgd.min.js';
+import './jquery.autocomplete.min.jsx';
+import './jquery.form.min.js';
+import './jquery.maskedinput.min.js';
+import './cropper.min.jsx';
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/js/theme.js',
+                'resources/sass/cropper.min.css'
             ],
             refresh: true,
         }),
